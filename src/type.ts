fd23d674382bee2bc097fc8f5a418722ce4992b2
Daniel <daniel.g@sps.co.il>
2025-08-24 12:27:16 +0300

    feat(type): add TypeFrom utility type for extracting object value types

--- src/type.ts
+++ src/type.ts
@@ -109,3 +109,11 @@ export function assertNumber(value: unknown): number {
 export function assertObject(value: unknown): object {
   return assertType(value, isObject);
 }
+
+/**
+ * @description Creates a type from the values of an object.
+ * @example
+ * const MyObject = { a: 'a', b: 'b' } as const;
+ * type MyObjectValues = TypeFrom<typeof MyObject>; // 'a' | 'b'
+ */
+export type TypeFrom<O extends object> = O[keyof O];
